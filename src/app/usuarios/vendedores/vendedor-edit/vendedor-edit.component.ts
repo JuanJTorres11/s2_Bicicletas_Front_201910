17d8943a109009cc209d2d805c5d5ca1833a91ec
Juan José Torres <jj.torresr@uniandes.edu.co>
2019-04-09 21:33:06 -0500

    inicio de vendedor-edit

--- src/app/usuarios/vendedores/vendedor-edit/vendedor-edit.component.ts
+++ src/app/usuarios/vendedores/vendedor-edit/vendedor-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { VendedorService } from '../vendedor.service';
+import { VendedorDetail } from '../vendedorDetail';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-vendedor-edit',
@@ -7,9 +11,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VendedorEditComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private service: VendedorService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private toastrService: ToastrService) { }
 
+  /**
+     * Vendedor que se mostrará
+     */
+  vendedor: VendedorDetail;
+
+  /**
+   * Identificador que llega en la ruta
+   */
+  id: number;
+
+  /**
+   * Obtiene el vendedor actual
+   */
+  getVendedorDetail(): void {
+    this.service.getVendedorDetail(this.id).subscribe((vendedorRemoto) => {
+      this.vendedor = vendedorRemoto
+    });
+  }
+
+  putVendedor():void {
+    this.service.putVendedor(this.id, this.vendedor).subscribe((vendedorRemoto) => {
+      this.vendedor = vendedorRemoto
+    });
+    this.toastrService.success('Se inició sesión correctamente');
+    this.router.navigateByUrl('/vendedores/' + this.id);
+  }
+
+  /**
+   * Método que se ejecuta cuando se crea el componente
+   */
   ngOnInit() {
+    this.id = +this.route.snapshot.paramMap.get('id');
+    if (this.id) {
+      this.vendedor = new VendedorDetail();
+      this.getVendedorDetail();
+    }
   }
 
 }
