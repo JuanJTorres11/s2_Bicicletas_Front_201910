5eba94bf94be457572a035e38bcd738b411cd260
Andres Donoso <af.donoso@uniandes.edu.co>
2019-04-11 14:22:44 -0500

    Modificado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import {OrdenModule} from './orden/orden.module'
 import { AuthModule } from './auth/auth.module';
 import { VendedoresModule } from './usuarios/vendedores/vendedores.module';
 import { CategoriaModule } from './categoria/categoria.module';
+import { MediopagoModule } from './mediopago/mediopago.module';
 
 @NgModule({
     declarations: [
@@ -46,7 +47,8 @@ import { CategoriaModule } from './categoria/categoria.module';
         MarcaModule,
         OrdenModule,
         AuthModule,
-        CategoriaModule
+        CategoriaModule,
+        MediopagoModule
     ],
     bootstrap: [AppComponent],
     providers: [
