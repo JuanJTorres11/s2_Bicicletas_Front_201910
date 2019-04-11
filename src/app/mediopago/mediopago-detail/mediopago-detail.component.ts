e009c6f4f82a28a20dd3979e427d7ca4617ab4fc
Andres Donoso <af.donoso@uniandes.edu.co>
2019-04-10 21:45:54 -0500

    Modificado

---
+++ src/app/mediopago/mediopago-detail/mediopago-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mediopago-detail',
+  templateUrl: './mediopago-detail.component.html',
+  styleUrls: ['./mediopago-detail.component.css']
+})
+export class MediopagoDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/mediopago/mediopago.module.ts
+++ src/app/mediopago/mediopago.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MediopagoListComponent } from './mediopago-list/mediopago-list.component';
+import { MediopagoAddComponent } from './mediopago-add/mediopago-add.component';
+import { MediopagoDetailComponent } from './mediopago-detail/mediopago-detail.component';
+import { MediopagoEditComponent } from './mediopago-edit/mediopago-edit.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [MediopagoListComponent, MediopagoAddComponent, MediopagoDetailComponent, MediopagoEditComponent]
 })
 export class MediopagoModule { }
