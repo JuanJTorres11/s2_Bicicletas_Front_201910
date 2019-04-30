6b73abe988e1a0cf8c270654dd7f5ad2090a40d3
Andres Donoso <af.donoso@uniandes.edu.co>
2019-04-30 10:39:06 -0500

    Agregado

---
+++ src/app/categoria/categoria-add-bicicleta/categoria-add-bicicleta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'categoria-add-bicicleta',
+  templateUrl: './categoria-add-bicicleta.component.html',
+  styleUrls: ['./categoria-add-bicicleta.component.css']
+})
+export class CategoriaAddBicicletaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
