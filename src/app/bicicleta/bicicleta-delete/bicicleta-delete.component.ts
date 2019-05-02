aa0eff05b4dbc04a873391d518c8edf2555e5724
Andrea Bayona <ma.bayona@uniandes.edu.co>
2019-05-01 20:07:46 -0500

    delete

---
+++ src/app/bicicleta/bicicleta-delete/bicicleta-delete.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bicicleta-delete',
+  templateUrl: './bicicleta-delete.component.html',
+  styleUrls: ['./bicicleta-delete.component.css']
+})
+export class BicicletaDeleteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
