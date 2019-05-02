ef532d380752705c6cc180fa9a086a574aefe634
Andrea Bayona <ma.bayona@uniandes.edu.co>
2019-05-01 20:44:34 -0500

    delete

--- src/app/bicicleta/bicicleta-delete-resena/bicicleta-delete-resena.component.ts
+++ src/app/bicicleta/bicicleta-delete-resena/bicicleta-delete-resena.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild, EventEmitter, Output, Input} from '@angular/core';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {DatePipe} from '@angular/common';
+import {Router, ActivatedRoute} from '@angular/router';
+import {NgbTypeahead} from '@ng-bootstrap/ng-bootstrap';
+import {Observable, Subject, merge} from 'rxjs';
+import {debounceTime, distinctUntilChanged, filter, map} from 'rxjs/operators';
+import {ToastrService} from 'ngx-toastr';
+
+import { Resena } from '../resena';
+import { BicicletaService } from '../bicicleta.service';
+import { Bicicleta } from '../../bicicleta/bicicleta';
 
 @Component({
   selector: 'app-bicicleta-delete-resena',
@@ -7,7 +18,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BicicletaDeleteResenaComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+  
+     private bicicletaService: BicicletaService,
+     private toastrService: ToastrService,
+     private router: Router,
+     private route: ActivatedRoute
+
+  ) { }
+
+  /**
+  *El id de la resena
+  */
+ @Input()resena_id: number;
+
+  /**
+  *El id de la bicicleta
+  */
+  @Input()bicicleta_id: number;
+
+
+  toggle(): void {
+        $('#eliminar'+this.resena_id).modal();
+    }
+
 
   ngOnInit() {
   }
