4ac9d928e5d2f0fb0f7ac573280088f5569382ef
Andrea Bayona <ma.bayona@uniandes.edu.co>
2019-05-12 21:22:04 -0500

    Filtros

--- src/app/bicicleta/bicicleta-filters/bicicleta-filters.component.ts
+++ src/app/bicicleta/bicicleta-filters/bicicleta-filters.component.ts
@@ -1,4 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Output, EventEmitter} from '@angular/core';
+import {Router} from '@angular/router';
+import {ToastrService} from 'ngx-toastr';
+
+import {BicicletaService} from '../bicicleta.service';
+import {Bicicleta} from '../bicicleta';
+
+import {MarcaService} from '../../marca/marca.service'; 
+import {CategoriaService} from '../../categoria/categoria.service'; 
+
+import {Categoria} from '../../categoria/categoria';
+import {Marca} from '../../marca/marca'; 
+
 
 declare var $: any; 
 
@@ -9,12 +21,59 @@ declare var $: any;
 })
 export class BicicletaFiltersComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+       private bicicletaService: BicicletaService,
+        private marcaService: MarcaService,
+        private categoriaService: CategoriaService,
+        private toastrService: ToastrService,
+        private router: Router
+  
+  ) { }
+
+  /**
+    * La lista de todas las categorias
+    */
+    categorias: Categoria[];
+
+   /**
+    * La lista de todas las marcas
+    */
+    marcas: Marca[];
+	 
+  	/**
+    * Retorna la lista de todas las marcas
+    */
+    getMarcas(): void {
+        this.marcaService.getMarcas()
+            .subscribe(marcas => {
+                this.marcas = marcas;
+            }, err => {
+                this.toastrService.error(err, 'Error');
+            });
+    }
+
+	/**
+    * Retorna la lista de todas las categorias
+    */
+    getCategorias(): void {
+        this.categoriaService.getCategorias()
+            .subscribe(categorias => {
+                this.categorias = categorias;
+            }, err => {
+                this.toastrService.error(err, 'Error');
+            });
+    }
+
+	 /**
+    * Funcion que incializa el componente
+    */
+    ngOnInit() {
+		this.getCategorias();
+		this.getMarcas();
+     }
 
 
-
-  ngOnInit() {
-  }
+ 
 
 }
 function SidebarCollapse () {
@@ -33,4 +92,7 @@ function SidebarCollapse () {
     
     // Collapse/Expand icon
     $('#collapse-icon').toggleClass('fa-angle-double-left fa-angle-double-right');
-}
+	
+	}
+
+
