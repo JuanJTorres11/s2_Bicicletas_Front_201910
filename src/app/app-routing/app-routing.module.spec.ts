b434c77b490cc6cad6377a5e04048b3b307d9189
Andrea Bayona <ma.bayona@uniandes.edu.co>
2019-05-28 13:49:49 -0500

    ..

--- src/app/app-routing/app-routing.module.spec.ts
+++ src/app/app-routing/app-routing.module.spec.ts
@@ -1,7 +1,7 @@
 import {AppRoutingModule} from './app-routing.module';
 
 describe('AppRoutingModule', () => {
-    let appRoutingModule: AppRoutingModule;
+    let appRoutingModule: AppRoutingModule; 
 
     beforeEach(() => {
         appRoutingModule = new AppRoutingModule();
